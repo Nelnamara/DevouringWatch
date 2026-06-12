9ba8269cdf71a4b5e50b96d848130d6117006664
Nelnamara <gwilkins@local>
2026-06-11 22:46:41 -0700

    Initial commit: DevouringWatch skeleton

---
+++ DevouringWatch.lua
@@ -0,0 +1,9 @@
+-- DevouringWatch
+-- Void Assault Escalation tracker for WoW Midnight 12.0.7+
+-- Author: Nelnamara
+
+DevouringWatch = {}
+local DW = DevouringWatch
+
+-- TODO: implement void world rotation timer, assault phase tracking,
+-- Field Accolades / Voidlight Marl progress display
